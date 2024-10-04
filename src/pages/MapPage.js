95965c340d97de0de77211ad6928791406b79a0e
hhwang1022 <hjd2110@naver.com>
2024-10-04 13:32:15 +0900

    카카오지도 추가

---
+++ src/pages/MapPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MemberMap from '../components/map/MemberMap';
+
+const MapPage = () => {
+    return (
+        <MemberMap/>
+    );
+};
+
+export default MapPage;
